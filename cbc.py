cee5a3b917a9894144efb6e7886d0aeeab46b5bb
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2019-12-29 02:14:53 +0200

    ok

--- cbc.py
+++ cbc.py
@@ -2,18 +2,10 @@ from sys import argv
 from Crypto.Cipher import AES
 import codecs
 
-def __pad(text):
-    text_length = len(text)
-    amount_to_pad = 16 - (text_length % 16)
-    if amount_to_pad == 0:
-        amount_to_pad = 16
-    self.pad = chr(amount_to_pad)
-    return text + self.pad * amount_to_pad
-
 def pad(plaintext):
-    print(plaintext)
+    # print(plaintext)
     padding_len = 16 - (len(plaintext) % 16)
-    print(padding_len)
+    # print(padding_len)
     if padding_len == 0:
         padding_len = 16
     # padd = chr(padding_len) * padding_len
@@ -22,6 +14,10 @@ def pad(plaintext):
     # return plaintext + padding
     padding = bytes([padding_len]) * padding_len
     return plaintext + padding
+
+def pkcs7_strip(data):
+    padding_length = data[-1]
+    return data[:- padding_length]
 
 def xor_for_char(input_bytes, key_input):
     index = 0
@@ -46,9 +42,43 @@ def encrypt_CBC(enc_, key):
         # print(IV)
     print(codecs.decode(codecs.encode(output, 'base64')).replace("\n", ""), end='') #print the encrypted text in base 64
 
+def encrypt_aes_128_cbc(msg, key):
+    result = b''
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    previous_ctxt_block = bytearray(16)
+    padded_ptxt = pad(msg)
+    nb_blocks = (int)(len(padded_ptxt) / 16) #calculate the number of blocks I've to iter through
+    for i in range(nb_blocks):
+        to_encrypt = xor_for_char(padded_ptxt[i * 16:(i + 1) * 16], previous_ctxt_block) #xor a block with IV
+        new_ctxt_block = cipher.encrypt(to_encrypt)
+        result += new_ctxt_block
+        # for the next iteration
+        previous_ctxt_block = new_ctxt_block
+    return result
+
+def decrypt_aes_128_cbc(ctxt, key):
+    result = b''
+    previous_ctxt_block = bytearray(16)
+    blocks = (int)(len(ctxt) / 16) #calculate the number of blocks I've to iter through
+    cipher = AES.new(key, AES.MODE_ECB)
+    for i in range(blocks):
+        block = ctxt[i * 16:(i + 1) * 16]
+        to_xor = cipher.decrypt(block)
+        result += xor_for_char(to_xor, previous_ctxt_block)
+        assert len(result) != 0
+        # for the next iteration
+        previous_ctxt_block = block
+    return pkcs7_strip(result)
+
 f = open("input.txt", "r")
 if f.mode == 'r':
     content = f.readlines()
     for line_content in content:
-        encrypt_CBC(bytes(line_content, "utf-8"), bytes("YELLOW SUBMARINE", "utf-8"))
+        # encrypt_CBC(bytes(line_content, "utf-8"), bytes("YELLOW SUBMARINE", "utf-8"))
+        x = encrypt_aes_128_cbc(bytes(line_content, "utf-8"), bytes("YELLOW SUBMARINE", "utf-8"))
+        print(codecs.decode(codecs.encode(x, 'base64')).replace("\n", ""), end='') #print the encrypted text in base 64
+        de = decrypt_aes_128_cbc(x, bytes("YELLOW SUBMARINE", "utf-8"))
+        print(codecs.decode(de), end='') #print the encrypted text in base 64
+        # print(de)
 f.close()

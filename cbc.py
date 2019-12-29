814e333d2216159b7b9ec94d6df5e2a9eee834b2
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2019-12-29 12:52:15 +0200

    split to blocks

--- cbc.py
+++ cbc.py
@@ -1,29 +1,52 @@
 from Crypto.Cipher import AES
+from math import ceil
 import codecs
 
-BLOCK_SIZE = 16
+BLOCK_SIZE = 16 #šifrēs 16 baitu blokos
 
 def pad(data):
     # Šī funkcija papildina tekstu, lai tas var sadalīties BLOCK_SIZE baitu blokos
-    # aizpilda nepieciešamus baitus atbilstoši PKCS7 algoritmam,
-    # kur katrā baitā ieraksta ciparu, kas ir klāt pierakstītu baitu skaits;
-    # tas vēlāk noderēs papildinātu baitu noņemšanai (sk. "unpad" funkciju zemāk),
-    # kas noteiks, cik baitus jānoņem, nolasot ciparu no pēdēja baita
+    # aizpilda nepieciešamus baitus atbilstoši PKCS7 algoritmam
     # https://en.wikipedia.org/wiki/Padding_(cryptography)#PKCS#5_and_PKCS#7
+
+    # izrēķina, kāds ir pēdējā bloka atlikušais garums un nepieciešamais papildinājuma garums
     last_block_length = len(data) % BLOCK_SIZE
     padding_length = BLOCK_SIZE - last_block_length
 
     # ja papildus baiti nav jāliek, tad jāpieliek vesels bloks :)
     # tas ir nepieciešams lai visu ziņojumu apstrāde notiktu pēc viena un tā paša algoritma
     if padding_length == 0: padding_length = BLOCK_SIZE 
+
+    # izveido papildinājumu, katrā baitā ierakstot ciparu, kas ir klāt pierakstītu baitu skaits;
+    # tas vēlāk noderēs papildinātu baitu noņemšanai (sk. "unpad" funkciju zemāk),
+    # kas noteiks, cik baitus jānoņem, nolasot ciparu no pēdēja baita
     padding = bytes([padding_length]) * padding_length
+
+    # atgriež tekstu, kas perfekti sadalās vajadzīgajos blokos
     return data + padding
 
 def unpad(data):
-    padding_lengthgth = data[-1]
-    return data[: -padding_lengthgth]
+    # nosaka, cik baiti jānoņem, nolasot ciparu no pēdēja baita
+    padding_length = data[-1]
 
-def bxor(a, b): return bytes([ x^y for (x,y) in zip(a, b)])
+    # atgriež datus, izņemot papildinājumu
+    return data[: -padding_length]
+
+def xor(a, b): 
+    # veic XOR katram bitam https://python-reference.readthedocs.io/en/latest/docs/operators/bitwise_XOR.html
+    # zip ir funkcija, kas izveido pārus pa elementriem no katra masīva
+    # https://www.geeksforgeeks.org/zip-in-python/    
+    pairs_of_bits = zip(a, b)
+    return bytes([ x^y for (x,y) in pairs_of_bits])
+
+def split_to_blocks(bytestring):
+    # Sadala blokos
+    number_of_blocks_not_rounded = len(bytestring) / BLOCK_SIZE # Aprēķina bloku skaitu
+    number_of_blocks = ceil(number_of_blocks_not_rounded) # Noapaļo bloku skaitu līdz veselajam, uz augšu
+    block_numbers = range(number_of_blocks) # masīvs ar bloku kārtas numuriem
+    # tagad katram bloka numuram "izgriezīs" atbilstošu bloku,
+    # un atgriezīs masīvu ar šiem blokiem
+    return [bytestring[BLOCK_SIZE*i : BLOCK_SIZE*(i+1)] for i in block_numbers]
 
 def encrypt_aes_128_cbc(msg, key):
     result = b''
@@ -34,9 +57,9 @@ def encrypt_aes_128_cbc(msg, key):
 
     previous_ctxt_block = bytearray(BLOCK_SIZE) #initializācijas vektors, BLOCK_SIZE nulles
     padded_ptxt = pad(msg)
-    nb_blocks = (int)(len(padded_ptxt) / BLOCK_SIZE) #calculate the number of blocks I've to iter through
-    for i in range(nb_blocks):
-        to_encrypt = bxor(padded_ptxt[i * BLOCK_SIZE:(i + 1) * BLOCK_SIZE], previous_ctxt_block) #xor a block with IV
+    blocks = split_to_blocks(padded_ptxt) #calculate the number of blocks I've to iter through
+    for block in blocks:
+        to_encrypt = xor(block, previous_ctxt_block) #xor a block with IV
         new_ctxt_block = cipher.encrypt(to_encrypt)
         result += new_ctxt_block
         previous_ctxt_block = new_ctxt_block
@@ -45,12 +68,11 @@ def encrypt_aes_128_cbc(msg, key):
 def decrypt_aes_128_cbc(ctxt, key):
     result = b''
     previous_ctxt_block = bytearray(BLOCK_SIZE)
-    blocks = (int)(len(ctxt) / BLOCK_SIZE) #calculate the number of blocks I've to iter through
     cipher = AES.new(key, AES.MODE_ECB)
-    for i in range(blocks):
-        block = ctxt[i * BLOCK_SIZE:(i + 1) * BLOCK_SIZE]
+    blocks = split_to_blocks(ctxt)
+    for block in blocks:
         to_xor = cipher.decrypt(block)
-        result += bxor(to_xor, previous_ctxt_block)
+        result += xor(to_xor, previous_ctxt_block)
         previous_ctxt_block = block
     return unpad(result)
 

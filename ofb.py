d71a9d22c089dde396f3bb5382609603da749aa3
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2019-12-30 01:57:43 +0200

    r

--- ofb.py
+++ ofb.py
@@ -7,7 +7,8 @@ import codecs
 # tam nav jāpieverš uzmanība; OFB piemērs drukās "Atšifrēja OFB: "
 # bet tīrībai var to arī novērst, cbc.py pēdējas divas rindiņas aizkomentējot
 # vai pirms viņām ierakstot `if __name__ == "__main__":`
-# laboto failu nesūtu, lai nebūtu sajaukumu
+# varēja arī vēl uzlabot, iznesot ārā kopīgas lietas
+# tomēr laboto failu nesūtu, lai nebūtu sajaukumu
 from cbc import read, write, getKey, pad, unpad, split_to_blocks, xor
 
 # Priekšnoteikumi: Python (sk. CBC.py sīkāk)
@@ -21,21 +22,17 @@ from cbc import read, write, getKey, pad, unpad, split_to_blocks, xor
 # Beigu rezultāts - divas funkcijas (encrypt, decrypt) un viņu izsaukumi
 
 def encrypt_ofb(plainText, key):
-    cipherTextChunks = []
-    iv = get_random_bytes(16)
-    originalIV = iv
+    # komentēšu lietas, kas atšķiras no CBC; cipher utml ir tāds pats kā CBC
     cipher = AES.new(key, AES.MODE_ECB)
-    plainText = pad(plainText)
-    blocks = split_to_blocks(plainText)
-
+    encrypted = b''
+    originalIV = get_random_bytes(16)
+    iv = originalIV
+    blocks = split_to_blocks(pad(plainText))
     for block in blocks:
         toXor = cipher.encrypt(iv)
-        toEnc = block
-        cipherText = xor(toXor, toEnc)
-        cipherTextChunks.append(cipherText)
+        encrypted += xor(toXor, block)
         iv = toXor
-
-    write("encrypted_ofb.txt", b''.join(cipherTextChunks))
+    write("encrypted_ofb.txt", encrypted)
     write("iv_ofb.txt", originalIV)
 
 def decrypt_ofb(cyphertext, key, iv):

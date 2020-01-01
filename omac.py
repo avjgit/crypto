2d395eecdd47fb3c294bcbb0a181178dabaa9c6b
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-01-01 10:29:19 +0200

    omac

--- omac.py
+++ omac.py
@@ -1,8 +1,8 @@
-
 from Crypto.Hash import CMAC
 from Crypto.Cipher import AES
-from cbc import read, write, getKey, xor
+from cbc import read, write, getKey, xor, split_to_blocks
 import codecs
+from math import ceil
 
 def generate_subkeys(key_K):
 #    Algoritms no https://www.ietf.org/rfc/rfc4493.txt Section 2.3.  Subkey Generation Algorithm
@@ -68,22 +68,112 @@ def generate_subkeys(key_K):
 
 # Atkal - loģika tā pati, kā K1 aprēķinā (tikai te L vietā ir K1),
 # varētu iznest atsevišķajā funkcijā, bet atstāju vienkāršībai un ērtākai sekošanai
-
     return K1, K2
 
-def get_omac():
-    # Divide message into b-bit blocks m = m1 ∥ ... ∥ mn−1 ∥ mn, where m1, ..., mn−1 are complete blocks. (The empty message is treated as one incomplete block.)
-    # If mn is a complete block then mn′ = k1 ⊕ mn else mn′ = k2 ⊕ (mn ∥ 10...02).
-    # Let c0 = 00...02.
-    # For i = 1, ..., n − 1, calculate ci = Ek(ci−1 ⊕ mi).
-    # cn = Ek(cn−1 ⊕ mn′)
-    # Output t = msbℓ(cn).
+def get_omac(key_K, message_M):
+#    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#    +                   Algorithm AES-CMAC                              +
+#    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#    +   Input    : K    ( 128-bit key )                                 +
+#    +            : M    ( message to be authenticated )                 +
+#    +            : len  ( length of the message in octets )             +
+#    +   Output   : T    ( message authentication code )                 +
+#    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#    +   Constants: const_Zero is 0x00000000000000000000000000000000     +
+#    +              const_Bsize is 16                                    +
+#    +                                                                   +
+#    +   Variables: K1, K2 for 128-bit subkeys                           +
+#    +              M_i is the i-th block (i=1..ceil(len/const_Bsize))   +
+#    +              M_last is the last block xor-ed with K1 or K2        +
+#    +              n      for number of blocks to be processed          +
+#    +              r      for number of octets of last block            +
+#    +              flag   for denoting if last block is complete or not +
+#    +                                                                   +
+#    +   Step 1.  (K1,K2) := Generate_Subkey(K);                         +
+#    +   Step 2.  n := ceil(len/const_Bsize);                            +
+#    +   Step 3.  if n = 0                                               +
+#    +            then                                                   +
+#    +                 n := 1;                                           +
+#    +                 flag := false;                                    +
+#    +            else                                                   +
+#    +                 if len mod const_Bsize is 0                       +
+#    +                 then flag := true;                                +
+#    +                 else flag := false;                               +
+#    +                                                                   +
+#    +   Step 4.  if flag is true                                        +
+#    +            then M_last := M_n XOR K1;                             +
+#    +            else M_last := padding(M_n) XOR K2;                    +
+#    +   Step 5.  X := const_Zero;                                       +
+#    +   Step 6.  for i := 1 to n-1 do                                   +
+#    +                begin                                              +
+#    +                  Y := X XOR M_i;                                  +
+#    +                  X := AES-128(K,Y);                               +
+#    +                end                                                +
+#    +            Y := M_last XOR X;                                     +
+#    +            T := AES-128(K,Y);                                     +
+#    +   Step 7.  return T;                                              +
+#    +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-    # K1 is used for the case where the length of
-    # the last block is equal to the block length.  K2 is used for the case
-    # where the length of the last block is less than the block length.
+# In step 1, subkeys K1 and K2 are derived from K through the subkey
+# generation algorithm.
+    CONST_ZERO_RFC  = 0x00000000000000000000000000000000
+    const_Zero = CONST_ZERO_RFC.to_bytes(16, byteorder='big')
+    const_Bsize = 16
+    K1, K2 = self.generate_subkey(key_K)
 
-    return "333beaa54a0dd3ab411a6352dd949601"
+# In step 2, the number of blocks, n, is calculated.  The number of
+# blocks is the smallest integer value greater than or equal to the
+# quotient determined by dividing the length parameter by the block
+# length, 16 octets.
+    n = ceil(len(message_M)/const_Bsize)
+
+    message_blocks = split_to_blocks(message_M)
+
+# In step 3, the length of the input message is checked.  If the input
+# length is 0 (null), the number of blocks to be processed shall be 1,
+# and the flag shall be marked as not-complete-block (false).
+# Otherwise, if the last block length is 128 bits, the flag is marked
+# as complete-block (true); else mark the flag as not-complete-block (false).
+    if n == 0:
+        n = 1
+        message_blocks.append(b"")
+        flag = False
+    else:
+        if (len(message_M) % const_Bsize) == 0:
+            flag = True
+        else:
+            flag = False
+
+# In step 4, M_last is calculated by exclusive-OR'ing M_n and one of
+# the previously calculated subkeys.  If the last block is a complete
+# block (true), then M_last is the exclusive-OR of M_n and K1.
+# Otherwise, M_last is the exclusive-OR of padding(M_n) and K2.
+    if flag == True:
+        M_last = xor(message_blocks[n-1], K1)
+    else:
+        # padding(x) is the concatenation of x and a single '1',
+        # followed by the minimum number of '0's, so that the total length is equal to 128 bits.
+        # (te - līdz 16 bitiem (len(message_blocks[n-1])), jo papildina tikai pēdējo bloku)
+        padding = b"\x80" + b"\x00"*(const_Bsize - len(message_blocks[n-1]) - 1 ) 
+        M_last = xor((message_blocks[n-1] + padding), K2)
+
+# In step 5, the variable X is initialized.
+    X = const_Zero
+
+# In step 6, the basic CBC-MAC is applied to M_1,...,M_{n-1},M_last.
+    cipher = AES.new(key_K, AES.MODE_ECB)
+
+    for i in range(n-1):
+        Y = xor(X, message_blocks[i])
+        X = cipher.encrypt(Y)
+    
+    Y = xor(M_last, X)
+    T = cipher.encrypt(Y)
+    
+# In step 7, the 128-bit MAC, T := AES-CMAC(K,M,len), is returned.
+    return T;
+
+    # return "333beaa54a0dd3ab411a6352dd949601"
 
 def get_mac_from_library(plainText, key):
     lib_cmac = CMAC.new(key, ciphermod=AES)

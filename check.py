da5a2efae7a5fcf02063ad8eec245fac0ef9bb97
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-01-01 11:43:26 +0200

    cleanup

--- check.py
+++
@@ -1,9 +0,0 @@
-const_Bsize = 6
-mlen = 2
-
-print(b"\x80" + b"\x00"*(const_Bsize - mlen - 1 ))
-print(bytes(1) + bytes(0)*(const_Bsize - mlen - 1 ))
-print(bytes([1])*4)
-print(bytes(0))
-print(bytes(4))
-print(chr(128))

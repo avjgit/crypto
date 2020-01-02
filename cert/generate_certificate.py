346d41c50c61b6576103497c30c89c0b973985b7
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-01-02 14:20:34 +0200

    cert

---
+++ cert/generate_certificate.py
@@ -0,0 +1,38 @@
+from OpenSSL import crypto, SSL
+from socket import gethostname
+from pprint import pprint
+from time import gmtime, mktime
+# Lai izmantotu OpenSSL, ir jāinstalē pyopenssl modulis
+# `python -m pip install pyopenssl`
+
+CERT_FILE = "self_signed.crt"
+KEY_FILE = "private.key"
+
+def create_self_signed_cert():
+
+    # create a key pair
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 1024)
+
+    # create a self-signed cert
+    cert = crypto.X509()
+    cert.get_subject().C = "UK"
+    cert.get_subject().ST = "London"
+    cert.get_subject().L = "London"
+    cert.get_subject().O = "Dummy Company Ltd"
+    cert.get_subject().OU = "Dummy Company Ltd"
+    cert.get_subject().CN = gethostname()
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10*365*24*60*60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha1')
+
+    with open(CERT_FILE, "wt") as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+
+    with open(KEY_FILE, "wt") as f:
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+
+create_self_signed_cert()

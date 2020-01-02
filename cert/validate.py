cade48d715c11dd6415aa3b7044c7d67a1c39892
Jurčenoks, Andrejs <andrejs.jurcenoks@if.lv>
2020-01-02 16:20:01 +0200

    validity

--- cert/validate.py
+++ cert/validate.py
@@ -1,14 +1,12 @@
-# from cryptography.hazmat.primitives.serialization import load_pem_public_key
-# from cryptography.hazmat.primitives.asymmetric import padding
-# from cryptography.hazmat.backends import default_backend
-
 from cryptography import x509
-from cryptography.hazmat.backends import default_backend
 from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.backends import default_backend
 
 # R: Write a program that verifies your certificate 
 # (since it is a root certificate, it is sufficient to check 
 # whether certificate issuer and subject are the same ...
+# and whether a digital signature of the issuer matches the subject’s public key
 # -----------------------------------------------------------------------------
 # Sertifikāta pārbaude - salīdzina issuer ar subject (self-signed sertifikātam)
 # -----------------------------------------------------------------------------
@@ -16,17 +14,20 @@ with open("certificate.pem", mode='rb') as file: certificateBytes = file.read()
 cert = x509.load_pem_x509_certificate(certificateBytes, default_backend())
 issuer = cert.issuer.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
 subject = cert.subject.get_attributes_for_oid(NameOID.ORGANIZATION_NAME)[0].value
-validity = "valīds" if issuer == subject else "nevalīds"
+issuerOk = issuer == subject
+# -----------------------------------------------------------------------------
+# Sertifikāta pārbaude - pārbauda parakstu
+# -----------------------------------------------------------------------------
+signatureOk = True
+try:
+    print(cert.public_key().verify(
+        cert.signature,
+        cert.tbs_certificate_bytes,
+        padding.PKCS1v15(),
+        cert.signature_hash_algorithm,
+    ))
+except:
+    signatureOk = False
+
+validity = "valīds" if issuerOk and signatureOk else "nevalīds"
 print(f'Sertifkāta izdevējs {issuer} sertificē {subject}. Self-signed sertifikāts ir {validity}.')
-
-# R: ...and whether a digital signature of the issuer matches the subject’s public key
-
-# issuer_public_key = load_pem_public_key(pem_issuer_public_key, default_backend())
-# cert_to_check = x509.load_pem_x509_certificate(pem_data_to_check, default_backend())
-# issuer_public_key.verify(
-#     cert_to_check.signature,
-#     cert_to_check.tbs_certificate_bytes,
-#     # Depends on the algorithm used to create the certificate
-#     padding.PKCS1v15(),
-#     cert_to_check.signature_hash_algorithm,
-# )
